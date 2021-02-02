4712049a424aba98f90936fb93f4d4519db448f4
wtkn25 <atrastar72@gmail.com>
2021-02-02 20:00:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Auth from './components/Auth';
+import MainPage from './components/MainPage';
+
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8000/graphql/',
+  headers: {
+    authorization: localStorage.getItem("token")
+      ? `JWT ${localStorage.getItem("token")}`
+      : ""
+  },
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="styles.app">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/employees" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
+
   );
 }
 
